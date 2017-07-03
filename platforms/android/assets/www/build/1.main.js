bd793024dc94aed25d9ea8ec4e36582e2994077a
Rodrigo <rodrigodemingo@gmail.com>
2017-07-03 10:41:55 +0400

    SVG

--- platforms/android/assets/www/build/1.main.js
+++ platforms/android/assets/www/build/1.main.js
@@ -1,12 +1,12 @@
 webpackJsonp([1],{
 
-/***/ 264:
+/***/ 266:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(27);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(23);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__register__ = __webpack_require__(99);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "RegisterPageModule", function() { return RegisterPageModule; });
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
